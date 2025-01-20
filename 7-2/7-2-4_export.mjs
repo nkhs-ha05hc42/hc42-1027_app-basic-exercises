ed560641d64ab4ede10258afa73fe19db8398804
hc421027 <nkhshc421027@g.neec.ac.jp>
2025-01-20 15:49:53 +0900

    7-2

---
+++ 7-2/7-2-4_export.mjs
@@ -0,0 +1,11 @@
+
+import { multiply } from './7-2-4.mjs';
+import { divide } from './7-2-4e.mjs';
+
+
+const multiplyResult = multiply(6, 3); // 掛け算
+const divideResult = divide(6, 3);     // 割り算
+
+
+console.log(`6 * 3 = ${multiplyResult}`); // 6 * 3 = 18
+console.log(`6 / 3 = ${divideResult}`);   // 6 / 3 = 2
---
+++ 7-2/7-2-4e.mjs
@@ -0,0 +1,6 @@
+export function divide(a, b) {
+    if (b === 0) {
+        return 'Error: Division by zero';
+    }
+    return a / b;
+}
